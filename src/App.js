25e08e3c0c009a8836629bb9488486509a6662fb
HafizSantosa <myhafizas@gmail.com>
2025-06-20 18:11:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import PasswordChecker from './components/PasswordChecker';
+import FileHasher from './components/FileHasher';
+import './App.css'; // Untuk styling App.js
 
 function App() {
+  const [activeTab, setActiveTab] = useState('password'); // 'password' atau 'file'
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Kriptografi: Fungsi Hash & Integritas </h1>
+        <p>Final Project ET234501 Kriptografi Tahun Ajaran 2024/2025 Genap </p>
       </header>
+
+      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      <main className="App-main">
+        {activeTab === 'password' && <PasswordChecker />}
+        {activeTab === 'file' && <FileHasher />}
+      </main>
+
+      <footer className="App-footer">
+        <p>&copy; 2024 Final Project Kriptografi</p>
+      </footer>
     </div>
   );
 }
