61cf9035be9dd465c954f15a10187b2a7f236507
irfanqs <irfanqobus.01@gmail.com>
2025-06-23 13:10:01 +0700

    fix frontend

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import PasswordChecker from './components/PasswordChecker';
-import FileHasher from './components/FileHasher';
-import './App.css'; // Untuk styling App.js
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import PasswordChecker from "./components/PasswordChecker";
+import FileHasher from "./components/FileHasher";
+import "./App.css"; // Untuk styling App.js
 
 function App() {
-  const [activeTab, setActiveTab] = useState('password'); // 'password' atau 'file'
+	const [activeTab, setActiveTab] = useState("password"); // 'password' atau 'file'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Kriptografi: Fungsi Hash & Integritas </h1>
-        <p>Final Project ET234501 Kriptografi Tahun Ajaran 2024/2025 Genap </p>
-      </header>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Kriptografi: Fungsi Hash & Integritas </h1>
+				<p>Final Project ET234501 Kriptografi Tahun Ajaran 2024/2025 Genap </p>
+			</header>
 
-      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
+			<Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
 
-      <main className="App-main">
-        {activeTab === 'password' && <PasswordChecker />}
-        {activeTab === 'file' && <FileHasher />}
-      </main>
+			<main className="App-main">
+				{activeTab === "password" && <PasswordChecker />}
+				{activeTab === "file" && <FileHasher />}
+			</main>
 
-      <footer className="App-footer">
-        <p>&copy; 2024 Final Project Kriptografi</p>
-      </footer>
-    </div>
-  );
+			<footer className="App-footer">
+				<p>&copy; 2025 Final Project Kriptografi</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
